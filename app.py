3a9e2fb0451f09e41bde06d486fba07290c9a875
Allen Cyril <allenalvc@gmail.com>
2021-05-27 19:58:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ hide_streamlit_style = """
             }</style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-import base64
+import base58
 import tweepy
 from textblob import TextBlob
 from wordcloud import WordCloud
